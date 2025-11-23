f99a3c09091bc3073e3768aa2acd9c3c8b58ec30
Gavino <gavinkilfoy@hotmail.com>
2025-11-22 23:26:56 -0800

    update ready and presence to work with both apikey and no apikey

--- events/clientReady.js
+++ events/clientReady.js
@@ -5,10 +5,9 @@
 const { updateServerStatusEmbeds } = require("../ptero/utils/updateServerStatusEmbeds");
 const { updateNodeStatusEmbeds } = require("../ptero/utils/updateNodeStatusEmbeds");
 const { pterodactyl } = require("../config.json");
-const { ActivityType } = require("discord.js");
-const Nodeactyl = require("nodeactyl");
 const { updatePresence } = require("../utils/updatePresence");
 const { getAppErrorMessage } = require("../ptero/utils/appErrors");
+const { isApplicationKeyValid } = require("../ptero/utils/serverUtils");
 
 module.exports = {
     
@@ -18,26 +17,20 @@ module.exports = {
     async execute(client) {
 
         try {
-            console.log(`Ready! Logged in as ${client.user.tag}`);
             updateServerStatusEmbeds(client, pterodactyl.SERVER_STATUS_UPDATE_INTERVAL);
-            updateNodeStatusEmbeds(client, pterodactyl.NODE_STATUS_UPDATE_INTERVAL);
 
-            // Set initial status
-            const pteroApp = new Nodeactyl.NodeactylApplication(
-                pterodactyl.domain,
-                pterodactyl.apiKey
-            );
-            const servers = await pteroApp.getAllServers();
-            const totalServers = servers.data.length;
-            client.user.setActivity(`${totalServers} servers on ${pterodactyl.company}`, {
-                type: ActivityType.Watching
-            });
-            setInterval(async () => {
-                updatePresence(client);
-            }, 5 * 60 * 1000); // Update every 5 minutes
-            console.log(`Status set: Watching ${totalServers} servers on ${pterodactyl.company}`);
+            const appKeyVaild = await isApplicationKeyValid();
+            if (appKeyVaild) {
+                updateNodeStatusEmbeds(client, pterodactyl.NODE_STATUS_UPDATE_INTERVAL);
+                console.log(`✅ Sucessfully authenticated with the pterodactyl application API for ${pterodactyl.company}.`);
+            } else {
+                console.warn("⚠️ The pterodactyl API key is invalid. Node status embeds will not be updated. Server status logs will also not function.");
+            }
+            updatePresence(client);
+
+            console.log(`✅ Ready and logged in as ${client.user.tag}`);
         } catch (error) {
-            console.error("Error in clientReady event:", getAppErrorMessage(error));
+            console.error("Error in clientReady event:", error);
         }
     },
 
--- utils/updatePresence.js
+++ utils/updatePresence.js
@@ -1,17 +1,54 @@
 const { ActivityType } = require("discord.js");
 const Nodeactyl = require("nodeactyl");
 const { pterodactyl } = require("../config.json");
+const { getAppErrorMessage } = require("../ptero/utils/appErrors");
+const { isApplicationKeyValid } = require("../ptero/utils/serverUtils");
+const fs = require("fs");
 
 async function updatePresence(client) {
-    const pteroApp = new Nodeactyl.NodeactylApplication(
-        pterodactyl.domain,
-        pterodactyl.apiKey
-    );
-    const servers = await pteroApp.getAllServers();
-    const totalServers = servers.data.length;
-    await client.user.setActivity(`${totalServers} servers on ${pterodactyl.company}`, {
-        type: ActivityType.Watching
-    });
+
+    try {
+        const appKeyVaild = await isApplicationKeyValid(pterodactyl.apiKey);
+        if (appKeyVaild) {
+            const pteroApp = new Nodeactyl.NodeactylApplication(
+                pterodactyl.domain,
+                pterodactyl.apiKey
+            );
+            const servers = await pteroApp.getAllServers();
+            const totalServers = servers.data.length || `0`;
+            await client.user.setActivity(`${totalServers} servers on ${pterodactyl.company}`, {
+                type: ActivityType.Watching
+            });
+            //console.log(`Updated presence to watch ${totalServers} servers on ${pterodactyl.company}.`);
+        } else {
+            let totalEmbeds = 0;
+            //read embed.json files to count total server status embeds
+            const dataDir = "./ptero/data";
+            if (fs.existsSync(dataDir)) {
+                const files = fs.readdirSync(dataDir);
+                for (const file of files) {
+                    if (file.startsWith("statusMessages") && file.endsWith(".json")) {
+                        const statusMessages = JSON.parse(fs.readFileSync(`${dataDir}/${file}`));
+                        totalEmbeds += statusMessages.length;
+                    } else if (file.startsWith("nodeStatusMessages") && file.endsWith(".json")) {
+                        const nodeStatusMessages = JSON.parse(fs.readFileSync(`${dataDir}/${file}`));
+                        totalEmbeds += nodeStatusMessages.length;
+                    }
+                }
+            }
+            client.user.setActivity(`${totalEmbeds} servers on ${pterodactyl.company}`, {
+                type: ActivityType.Watching
+            });
+            //console.log(`Set presence to watch ${totalEmbeds} servers on ${pterodactyl.company}.`);
+        }
+
+        setInterval(async () => {
+            await updatePresence(client);
+        }, 5 * 60 * 1000); // Update every 5 minutes
+    } catch (error) {
+        console.error("Error updating presence:", getAppErrorMessage(error));
+    }
+
 }
 
 module.exports = {
